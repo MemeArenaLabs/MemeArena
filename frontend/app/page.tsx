09468a42d8fd9f3a34c6228479eb3f026e92b8da
Lucianosc <scaminaci.luciano@gmail.com>
2024-09-11 17:29:10 -0300

    Landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 import Link from "next/link";
 import { Button } from "../components/Button";
+import { useState } from "react";
+import { Modal } from "../components/Modal";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <main>
       <section className="layout">
-        <h2> Landing page</h2>
+        <h2>Landing page</h2>
         <Link href={"/app"}>
           <Button>Launch App</Button>
         </Link>
+        <Button onClick={() => setIsModalOpen(true)}>Open Modal</Button>
+        <Modal
+          onClose={() => setIsModalOpen(false)}
+          isOpen={isModalOpen}
+          title="Example modal"
+        >
+          Body
+        </Modal>
       </section>
     </main>
   );
