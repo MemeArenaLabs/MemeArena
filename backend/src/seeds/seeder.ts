5f0ec40a1440320939060678f962fdfa26355b5c
francoperez03 <franco.perez03@gmail.com>
2024-09-20 12:12:52 -0300

    chore: change clear to delete

--- backend/src/seeds/seeder.ts
+++ backend/src/seeds/seeder.ts
@@ -10,38 +10,22 @@ import { seedUserMemes } from './user-meme.seeder';
 import { User } from '../modules/user/user.entity';
 import { Meme, UserMeme, Skill } from '../modules/meme/meme.entity';
 import { Token } from '../modules/token/token.entity';
+import { BattleSessionUserMeme } from '../modules/battle/battle.entity';
 
 export async function seedDatabase(dataSource: DataSource) {
   console.log('start')
   try{
-    console.log('1')
-    await dataSource.manager.clear(UserMeme);
-    console.log('2')
-
-    await dataSource.manager.clear(User);
-    console.log('3')
-
-    await dataSource.manager.clear(Skill);
-    console.log('4')
-
-    await dataSource.manager.clear(Meme);
-    console.log('5')
-
-    await dataSource.manager.clear(Token);
-    console.log('6')
+    await dataSource.manager.delete(BattleSessionUserMeme, {});
+    await dataSource.manager.delete(UserMeme, {});
+    await dataSource.manager.delete(Skill, {});
+    await dataSource.manager.delete(Meme, {});
+    await dataSource.manager.delete(Token, {});
+    await dataSource.manager.delete(User, {});
   
     const tokens = await seedTokens(dataSource);
-    console.log('7')
-
     const memes = await seedMemes(dataSource, tokens);
-    console.log('8')
-
     await seedSkills(dataSource, memes);
-    console.log('9')
-
     const users = await seedUsers(dataSource);
-    console.log('10')
-
     await seedUserMemes(dataSource, users, memes);
   }
   catch(error){
