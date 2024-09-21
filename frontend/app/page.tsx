922c5c4b1bd13dad40eb41245a1cb7304caa38bf
Lucianosc <scaminaci.luciano@gmail.com>
2024-09-21 19:25:32 -0300

    add base colors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -21,13 +21,27 @@ export default function Home() {
         >
           Body
         </Modal>
-        <h1>Heading1 test 96</h1>
-        <h2>Heading2 test 64</h2>
-        <h3>Heading3 test 32</h3>
-        <p>paragraph test 16</p>
-        <p className="text-[14px]">paragraph test 14</p>
-        <p className="text-[12px]">paragraph test 12</p>
-        <p className="text-[10px]">paragraph test 10</p>
+        <div>
+          <h1>Heading1 test 96</h1>
+          <h2>Heading2 test 64</h2>
+          <h3>Heading3 test 32</h3>
+          <p>paragraph test 16</p>
+          <p className="text-[14px]">paragraph test 14</p>
+          <p className="text-[12px]">paragraph test 12</p>
+          <p className="text-[10px]">paragraph test 10</p>
+        </div>
+        <div className="flex flex-col gap-2 p-6 bg-[#444444]">
+          <div className="flex gap-2">
+            <div className="h-20 w-20 bg-yellow"></div>
+            <div className="h-20 w-20 bg-dark-blue"></div>
+            <div className="h-20 w-20 bg-dark-blue-80"></div>
+          </div>
+          <div className="flex gap-2">
+            <div className="h-20 w-20 bg-dark-blue-50"></div>
+            <div className="h-20 w-20 bg-blue-70"></div>
+            <div className="h-20 w-20 bg-light-blue"></div>
+          </div>
+        </div>
       </section>
     </main>
   );
