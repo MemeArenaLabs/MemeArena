1d7cd953d155fa899d346b6d988825bab6f141ef
cryptonahue <cryptonahue@gmail.com>
2024-10-01 12:33:51 -0300

    feat: battle preparation

--- frontend/app/app/battle-preparation/page.tsx
+++ frontend/app/app/battle-preparation/page.tsx
@@ -1,5 +1,319 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import SvgIcon from "@/utils/SvgIcon";
+import { Modal } from "@/components/Modal";
+import { TeamModal } from "@/components/gui/TeamModal";
+import { BottomMenu } from "@/components/gui/BottomMenu";
 
 export default function BattlePreparation() {
-  return <div>BattlePreparation</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isNewTeamModalOpen, setIsNewTeamModalOpen] = useState(false);
+  const [isEditTeamModalOpen, setIsEditTeamModalOpen] = useState(false);
+  const [newTeamName, setNewTeamName] = useState("NEW TEAM");
+  const [editTeamName, setEditTeamName] = useState("TEAM NAME");
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const openNewTeamModal = () => setIsNewTeamModalOpen(true);
+  const closeNewTeamModal = () => setIsNewTeamModalOpen(false);
+
+  const openEditTeamModal = () => setIsEditTeamModalOpen(true);
+  const closeEditTeamModal = () => setIsEditTeamModalOpen(false);
+
+  const handleSaveNewTeamName = (name: string) => {
+    setNewTeamName(name);
+    // Aquí puedes agregar lógica adicional para guardar el nuevo nombre del equipo
+  };
+
+  const handleSaveEditTeamName = (name: string) => {
+    setEditTeamName(name);
+    // Aquí puedes agregar lógica adicional para guardar el nombre editado del equipo
+  };
+
+  return (
+    <main className="flex flex-col text-white bg-cover bg-center h-[430px] w-[932px] bg-[url('/assets/team-selection/bg/bg.png')]">
+      <div className="flex flex-grow">
+        <div className="pt-2 pl-2 ">
+          {/* User CTA profile */}
+          <div className="pb-2 flex items-center">
+            <div className=" cursor-pointer flex gap-2 items-center p-1 w-40 bg-dark-blue-80 h-[43px] clip-path-polygon-left-gui-profile-selection ">
+              <div>
+                <Image
+                  src="/assets/team-selection/avatar/example-avatar.png "
+                  width={26.5}
+                  height={26.5}
+                  alt="Avatar"
+                />
+              </div>
+              <div className="text-[14px] font-bold">Magaibero22</div>
+            </div>
+            <div className="cursor-pointer w-14 pl-1 bg-dark-blue-80 h-[43px] ml-[-5px] clip-path-polygon-left-gui-profile-setting flex items-center justify-center">
+              <Image
+                src="/icons/settings.svg"
+                width={24}
+                height={24}
+                alt="Setting"
+              />
+            </div>
+          </div>
+
+          {/* Left side and Center - Characters */}
+          <div className="flex justify-center items-center h-full w-[500px] ">
+            <div>
+              <div className="flex gap-2  w-[400px] overflow-x-scroll overflow-y-hidden pb-4  ">
+                <div className="w-32 h-[110px] relative group cursor-pointer">
+                  <div className="absolute inset-0 z-20 transition-shadow duration-300 group-hover:shadow-[inset_0_0_0_4px_#FFFF00]"></div>
+
+                  <div className="w-32  h-full">
+                    <Image
+                      src="/assets/team-selection/gladiators/teams.png"
+                      width={128}
+                      height={110}
+                      alt="Gladiators"
+                    />
+
+                    <div className="w-32 flex items-center justify-center h-[27px] bg-[#05345A] bg-opacity-70">
+                      <div className="text-[10px] font-bold text-white">
+                        MEMEKILLERS
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-32 h-[110px] relative group cursor-pointer">
+                  <div className="absolute inset-0 z-20 transition-shadow duration-300 group-hover:shadow-[inset_0_0_0_4px_#FFFF00]"></div>
+
+                  <div className="w-32  h-full">
+                    <Image
+                      src="/assets/team-selection/gladiators/teams.png"
+                      width={128}
+                      height={110}
+                      alt="Gladiators"
+                    />
+
+                    <div className="w-32 flex items-center justify-center h-[27px] bg-[#05345A] bg-opacity-70">
+                      <div className="text-[10px] font-bold text-white">
+                        MEMEKILLERS
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-2">
+                <button className="bg-yellow text-black font-bold text-[14px] h-10 w-[367px] flex items-center justify-center gap-1">
+                  <SvgIcon name="all-for-one" className="text-dark h-5 w-5  " />
+                  I’M READY
+                </button>
+              </div>
+            </div>
+
+            {/*   <div>
+              <div>
+                <h2 className="relative mt-[-8px] ">MEMEKILLERS </h2>
+              </div>
+
+              <div className="flex w-[600px] justify-center pt-2">
+                <div className="w-full">
+                  <div className="grid  mx-auto w-full  justify-center h-40">
+                    <div className="flex max-w-[498px] ">
+                      <div className="mr-[-50px] ">
+                        <Image
+                          className="relative player z-40  "
+                          src="/assets/battle-layout/gladiators/magaiba.png"
+                          width={226}
+                          height={226}
+                          alt="Jugador"
+                        />
+                      </div>
+                      <div>
+                        <Image
+                          className="relative player z-40   "
+                          src="/assets/battle-layout/gladiators/magaiba.png"
+                          width={226}
+                          height={226}
+                          alt="Jugador"
+                        />
+                      </div>
+                      <div className="ml-[-50px]">
+                        <Image
+                          className="relative player z-40   "
+                          src="/assets/battle-layout/gladiators/magaiba.png"
+                          width={226}
+                          height={226}
+                          alt="Jugador"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex justify-center">
+                      {" "}
+                      <Image
+                        className="mt-[-50px] animate-pulse z-0"
+                        src="/assets/team-selection/bg/big-shadow.png"
+                        width={471}
+                        height={72}
+                        alt="Sombra jugador"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </div>
+
+        {/* Right side - Skills */}
+        <div className="w-full flex justify-end">
+          <div className="w-[200px]  bg-[url('/assets/team-selection/bg/bg-gladiators.png')] z-0 ">
+            <div className="pr-2 pt-2">
+              <div className="flex justify-end py-2">
+                <p> GLADIATORS</p>
+              </div>
+
+              <div className="flex justify-end">
+                <div className="grid grid-cols-1 gap-1  ">
+                  <div className=" flex items-center">
+                    <div
+                      className="w-full max-w-[98px] border-2 border-transparent hover:border-2 hover:border-yellow"
+                      onClick={openModal}
+                    >
+                      <div className=" w-[94px] h-[100px]  cursor-pointer  border-4 border-dark-blue-70 border-opacity-70  ">
+                        <div className=" z-10 h-full flex flex-col justify-between bg-[url('/assets/team-selection/gladiators/mog.png')]">
+                          <div className="flex justify-end p-1">
+                            <Image
+                              src="/assets/battle-layout/gui-skills/info-skills.svg"
+                              width={12}
+                              height={12}
+                              alt="Skill Info"
+                              className="cursor-pointer"
+                            />
+                          </div>
+                          <div className="w-full bg-[#05345A] h-[17px] bg-opacity-70 p-1">
+                            <div className="text-xs font-bold text-white text-center">
+                              MOG
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className=" flex items-center">
+                    <div
+                      className="w-full max-w-[98px] border-2 border-transparent hover:border-2 hover:border-yellow"
+                      onClick={openModal}
+                    >
+                      <div className=" w-[94px] h-[100px]  cursor-pointer  border-4 border-dark-blue-70 border-opacity-70  ">
+                        <div className=" z-10 h-full flex flex-col justify-between bg-[url('/assets/team-selection/gladiators/mog.png')]">
+                          <div className="flex justify-end p-1">
+                            <Image
+                              src="/assets/battle-layout/gui-skills/info-skills.svg"
+                              width={12}
+                              height={12}
+                              alt="Skill Info"
+                              className="cursor-pointer"
+                            />
+                          </div>
+                          <div className="w-full bg-[#05345A] h-[17px] bg-opacity-70 p-1">
+                            <div className="text-xs font-bold text-white text-center">
+                              MOG
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" flex items-center">
+                    <div
+                      className="w-full max-w-[98px] border-2 border-transparent hover:border-2 hover:border-yellow"
+                      onClick={openModal}
+                    >
+                      <div className=" w-[94px] h-[100px]  cursor-pointer  border-4 border-dark-blue-70 border-opacity-70  ">
+                        <div className=" z-10 h-full flex flex-col justify-between bg-[url('/assets/team-selection/gladiators/mog.png')]">
+                          <div className="flex justify-end p-1">
+                            <Image
+                              src="/assets/battle-layout/gui-skills/info-skills.svg"
+                              width={12}
+                              height={12}
+                              alt="Skill Info"
+                              className="cursor-pointer"
+                            />
+                          </div>
+                          <div className="w-full bg-[#05345A] h-[17px] bg-opacity-70 p-1">
+                            <div className="text-xs font-bold text-white text-center">
+                              MOG
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <BottomMenu />
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="SKILL INFORMATION"
+      >
+        <div className=" flex gap-2 max-w-[617px]">
+          <div className="min-w-[226px]">
+            <Image
+              src="/assets/skills/magaiba/skills-info-kiss-of-death.png"
+              width={226}
+              height={226}
+              alt="Kiss of Death"
+            />
+          </div>
+          <div className="p-2 max-w-[347px]">
+            <h3 className="text-yellow pb-2">KISS OF DEATH</h3>
+            <p className="font-normal text-[14px] ">
+              Magaiba unleashes a venomous tongue lash towards its opponent,
+              loaded with paralyzing toxins. With lethal precision, the strike
+              immobilizes the adversary, leaving them unable to move for a full
+              turn.
+            </p>
+            <div>
+              <ul className="text-[14px] font-bold p-2">
+                <li className="list-disc">Immobilizes the enemy for 1 turn.</li>
+                <li className="list-disc">
+                  Instant application with a high chance of paralysis.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
+      <TeamModal
+        initialTitle={newTeamName}
+        isOpen={isNewTeamModalOpen}
+        onClose={closeNewTeamModal}
+        onSave={handleSaveNewTeamName}
+      >
+        <div>
+          {/* Añade aquí los componentes necesarios para crear un nuevo equipo */}
+        </div>
+      </TeamModal>
+
+      <TeamModal
+        initialTitle={editTeamName}
+        isOpen={isEditTeamModalOpen}
+        onClose={closeEditTeamModal}
+        onSave={handleSaveEditTeamName}
+      >
+        <div></div>
+        {/* Añade aquí los componentes necesarios para editar el equipo */}
+      </TeamModal>
+    </main>
+  );
 }
--- frontend/components/gui/BottomMenu.tsx
+++ frontend/components/gui/BottomMenu.tsx
@@ -48,7 +48,7 @@ export function BottomMenu() {
           </button>
         ))}
         <button
-          onClick={() => navigate("/battle/selection")}
+          onClick={() => navigate("/app/battle-preparation")}
           className="clip-path-buttom-bar-right w-full h-[40px] bg-yellow hover:bg-yellow-600 text-black flex justify-center hover:opacity-85 font-bold py-2 transition-colors duration-200"
         >
           <span className="mr-2">
