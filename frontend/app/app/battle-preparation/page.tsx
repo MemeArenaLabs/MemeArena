c026c78cd486c5eb461e725e018e7e04fe115e31
cryptonahue <cryptonahue@gmail.com>
2024-10-05 11:24:05 -0300

    fix: little fixes

--- frontend/app/app/battle-preparation/page.tsx
+++ frontend/app/app/battle-preparation/page.tsx
@@ -3,9 +3,9 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import SvgIcon from "@/utils/SvgIcon";
-import Profile from "@/components/ProfilePanel";
+import ProfilePanel from "@/components/ProfilePanel";
 
-type TokenType = 'BEAR' | 'MAGAIBA' | 'MOG';
+type TokenType = "BEAR" | "MAGAIBA" | "MOG";
 
 const TOKEN_PRICES: Record<TokenType, number> = {
   BEAR: 0.000000021837,
@@ -14,15 +14,15 @@ const TOKEN_PRICES: Record<TokenType, number> = {
 };
 
 const STAKED_AMOUNTS: Record<TokenType, number> = {
-  BEAR: 123456733.2,
-  MAGAIBA: 3456789.2,
-  MOG: 123456789.2,
+  BEAR: 123456733,
+  MAGAIBA: 3456789,
+  MOG: 123456789,
 };
 
 const TOKEN_IMAGES: Record<TokenType, string> = {
-  BEAR: 'bear.png',
-  MAGAIBA: 'magaiba.png',
-  MOG: 'mog.png',
+  BEAR: "bear.png",
+  MAGAIBA: "magaiba.png",
+  MOG: "mog.png",
 };
 
 interface Gladiator {
@@ -38,9 +38,15 @@ interface Team {
 const DEFAULT_TEAM: Team = {
   name: "DEFAULT TEAM",
   gladiators: [
-    { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-    { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
-    { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
+    {
+      token: "BEAR",
+      image: "/assets/team-selection/gladiators/bear-no-bg.png",
+    },
+    {
+      token: "MAGAIBA",
+      image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+    },
+    { token: "MOG", image: "/assets/team-selection/gladiators/mog-no-bg.png" },
   ],
 };
 
@@ -48,85 +54,165 @@ const TEAMS: Team[] = [
   {
     name: "TEAM A",
     gladiators: [
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM B",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM C",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM D",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM E",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM F",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM G",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
   {
     name: "TEAM H",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
 
   {
     name: "TEAM I ",
     gladiators: [
-      { token: 'MOG', image: '/assets/team-selection/gladiators/mog-no-bg.png' },
-      { token: 'BEAR', image: '/assets/team-selection/gladiators/bear-no-bg.png' },
-      { token: 'MAGAIBA', image: '/assets/team-selection/gladiators/magaiba-no-bg.png' },
+      {
+        token: "MOG",
+        image: "/assets/team-selection/gladiators/mog-no-bg.png",
+      },
+      {
+        token: "BEAR",
+        image: "/assets/team-selection/gladiators/bear-no-bg.png",
+      },
+      {
+        token: "MAGAIBA",
+        image: "/assets/team-selection/gladiators/magaiba-no-bg.png",
+      },
     ],
   },
 ];
 
 export default function BattlePreparation() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedTeam, setSelectedTeam] = useState<Team>(TEAMS[0] || DEFAULT_TEAM);
+  const [selectedTeam, setSelectedTeam] = useState<Team>(
+    TEAMS[0] || DEFAULT_TEAM
+  );
   const [bets, setBets] = useState<number[]>(new Array(3).fill(0));
   const [totalBets, setTotalBets] = useState(0);
-
-  const openModal = () => setIsModalOpen(true);
 
   const calculateStakedValue = (token: TokenType) => {
     return STAKED_AMOUNTS[token] * TOKEN_PRICES[token];
@@ -159,15 +245,15 @@ export default function BattlePreparation() {
     setTotalBets(total);
   }, [bets]);
 
+  function openModal(): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <main className="flex flex-col text-white bg-cover bg-center w-[932px] bg-[url('/assets/team-selection/bg/bg.png')]">
       <div className="flex justify-between    ">
         <div className="pt-2 pl-2 ">
-          <Profile
-            username="YourUsername"
-            showSettings={true}
-            onOpenModal={openModal}
-          />
+          <ProfilePanel />
 
           <div className="flex   h-full px-10">
             <div className="pt-11 ">
@@ -180,21 +266,23 @@ export default function BattlePreparation() {
                     <div
                       key={index}
                       className={`w-[122px] h-28 relative group cursor-pointer ${
-                        selectedTeam.name === team.name 
-                          ? 'shadow-[inset_0_0_0_4px_#FFFF00]' 
-                          : 'hover:shadow-[inset_0_0_0_4px_#FFFF00]'
+                        selectedTeam.name === team.name
+                          ? "shadow-[inset_0_0_0_4px_#FFFF00]"
+                          : "hover:shadow-[inset_0_0_0_4px_#FFFF00]"
                       }`}
                       onClick={() => handleTeamSelect(team)}
                     >
-                      <div className={`absolute inset-0 z-20 transition-shadow duration-300 ${
-                        selectedTeam.name === team.name 
-                          ? 'shadow-[inset_0_0_0_4px_#FFFF00]' 
-                          : 'group-hover:shadow-[inset_0_0_0_4px_#FFFF00]'
-                      }`}></div>
+                      <div
+                        className={`absolute inset-0 z-20 transition-shadow duration-300 ${
+                          selectedTeam.name === team.name
+                            ? "shadow-[inset_0_0_0_4px_#FFFF00]"
+                            : "group-hover:shadow-[inset_0_0_0_4px_#FFFF00]"
+                        }`}
+                      ></div>
                       <div className="h-28 w-[122px]     ">
                         <div className="z-10  w-full h-full flex flex-col bg-no-repeat justify-between bg-[url('/assets/team-selection/gladiators/teams-x2.png')]">
                           <div className="flex justify-end  "></div>
-                          <div className="w-full  bg-dark-blue-70 p-1">    
+                          <div className="w-full  bg-dark-blue-70 p-1">
                             <div className="text-[10px] font-bold text-white text-center">
                               {team.name}
                             </div>
@@ -204,7 +292,9 @@ export default function BattlePreparation() {
                     </div>
                   ))
                 ) : (
-                  <div className="col-span-3 text-center">No teams available</div>
+                  <div className="col-span-3 text-center">
+                    No teams available
+                  </div>
                 )}
               </div>
             </div>
@@ -220,7 +310,10 @@ export default function BattlePreparation() {
 
               <div className="flex gap-2 items-center justify-between">
                 {Object.entries(STAKED_AMOUNTS).map(([token, amount]) => (
-                  <div key={token} className="flex gap-2 items-center justify-between">
+                  <div
+                    key={token}
+                    className="flex gap-2 items-center justify-between"
+                  >
                     <div className="w-7 h-7">
                       <Image
                         src={`/assets/logos/${TOKEN_IMAGES[token as TokenType]}`}
@@ -263,18 +356,26 @@ export default function BattlePreparation() {
                           />
                         </div>
                         <div className="bg-dark-blue-70 mt-[-20px] w-full z-0 flex justify-center">
-                          <p className="font-bold text-[10px]">{gladiator.token}</p>
+                          <p className="font-bold text-[10px]">
+                            {gladiator.token}
+                          </p>
                         </div>
                         <div className="py-1">
                           <div className="flex items-center justify-between gap-1">
-                            <div><p className="text-xl font-bold">$</p></div>
-                            <div><input
-                              type="text"
-                              value={bets[index] || ''}
-                              onChange={(e) => handleBetChange(index, e.target.value)}
-                              className="w-full bg-transparent outline-none text-white placeholder:text-white text-xl font-bold"
-                              placeholder="0.0"
-                            /></div>
+                            <div>
+                              <p className="text-xl font-bold">$</p>
+                            </div>
+                            <div>
+                              <input
+                                type="text"
+                                value={bets[index] || ""}
+                                onChange={(e) =>
+                                  handleBetChange(index, e.target.value)
+                                }
+                                className="w-full bg-transparent outline-none text-white placeholder:text-white text-xl font-bold"
+                                placeholder="0.0"
+                              />
+                            </div>
                           </div>
                         </div>
                         <div className="flex items-center gap-1">
@@ -288,7 +389,12 @@ export default function BattlePreparation() {
                           </div>
                           <div>
                             <p className="text-light-blue font-medium text-[10px]">
-                              {calculateTokenAmount(bets[index] || 0, TOKEN_PRICES[gladiator.token]).toLocaleString(undefined, {maximumFractionDigits: 2})}
+                              {calculateTokenAmount(
+                                bets[index] || 0,
+                                TOKEN_PRICES[gladiator.token]
+                              ).toLocaleString(undefined, {
+                                maximumFractionDigits: 2,
+                              })}
                             </p>
                           </div>
                         </div>
@@ -302,7 +408,9 @@ export default function BattlePreparation() {
                     <p className="text-xl uppercase">Total bets</p>
                   </div>
                   <div>
-                    <p className="text-3xl text-yellow">${totalBets.toFixed(2)}</p>
+                    <p className="text-3xl text-yellow">
+                      ${totalBets.toFixed(2)}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -318,4 +426,4 @@ export default function BattlePreparation() {
       </div>
     </main>
   );
-}   
+}
