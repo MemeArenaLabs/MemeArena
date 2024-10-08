5b23f08f9b5f89ceb617edee3d35465e334d21e5
Lucianosc <luchotandil90@gmail.com>
2024-10-08 09:59:19 -0300

    assets fix

--- frontend/utils/getGladiatorAssets.ts
+++ frontend/utils/getGladiatorAssets.ts
@@ -1,3 +1,11 @@
 export const getGladiatorImgUri = (tokenSymbol: string) => {
-  return `/assets/gladiators/${tokenSymbol.toLowerCase()}.png`;
+  return `/assets/gladiators/no-bg/${tokenSymbol.toLowerCase()}.png`;
+};
+
+export const getGladiatorColosseumBgImgUri = (tokenSymbol: string) => {
+  return `/assets/gladiators/pfp/${tokenSymbol.toLowerCase()}.png`;
+};
+
+export const getGladiatorSkillImgUri = (skillName: string) => {
+  return `/assets/gladiators/skills/${skillName}.png`;
 };
